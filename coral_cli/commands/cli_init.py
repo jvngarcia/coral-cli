d19d3001b69c6046158b34b42d0bd0ad3daf2b91
jvngarcia <angelgarciaweb@gmail.com>
2024-07-26 14:12:08 -0400

    feat: Add CLI initialization command

---
+++ coral_cli/commands/cli_init.py
@@ -0,0 +1,39 @@
+import click
+import os
+from string import Template
+
+
+template = Template("""
+# $name
+# $description
+import click
+
+@click.command()
+def cli():
+  click.echo("Hello World!")
+
+if __name__ == "__main__":
+  cli()
+""")
+
+
+@click.command(name='init')
+@click.option('--name', '-n', help='Name of the project', required=True, type=click.STRING)
+@click.option('--description', '-d', help='Description of the project', required=False, type=click.STRING)
+def cli_init( name: str, description: str ):
+  """
+  Initialize your CLI application
+  """
+  click.echo(f'Initializing the CLI application: {name}')
+  
+  
+  # Si no existe el directorio, lo creamos
+  if not os.path.exists(name):
+    os.makedirs(name)
+  
+  # Crear archivo main.py
+  with open(f'{name}/main.py', 'w') as f:
+    f.write(template.substitute(name=name, description=description))
+  
+  
+  click.echo('Done!')
