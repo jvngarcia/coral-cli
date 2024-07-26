50627fe6ba7da0335be9ebc3f8b57bf1e0bdc15a
jvngarcia <angelgarciaweb@gmail.com>
2024-07-26 14:11:52 -0400

    chore: Add subcommands to CLI module

--- coral_cli/cli.py
+++ coral_cli/cli.py
@@ -1,4 +1,15 @@
 import click
+import importlib
+from pathlib import Path
+import re
+
+
+
+
+SUBCOMMAND_DIR = Path("coral_cli/commands")
+
+
+
 
 @click.group()
 def cli():
@@ -7,8 +18,25 @@ def cli():
   """
   pass
 
+def add_subcommands(maincommand=cli):
+  print(f"Looking for subcommands in {SUBCOMMAND_DIR.glob('*.py')}")
+  for modpath in SUBCOMMAND_DIR.glob('*.py'):
+    print(f"Found {modpath}")
+    modname = re.sub(f'/', '.',  str(modpath)).rpartition('.py')[0]
+    
+    # Replace backslash with dot
+    modname = modname.replace('\\', '.')
+    
+    print(f"Importing {modname}")
+    mod = importlib.import_module(modname)
+    # Filter out any things that aren't a click Command
+    for attr in dir(mod):
+      foo = getattr(mod, attr)
+      if callable(foo) and isinstance(foo, click.core.Command):
+        maincommand.add_command(foo)
+
 
 def init():
-  # Import all the commands here
+  add_subcommands()
   cli()
   
