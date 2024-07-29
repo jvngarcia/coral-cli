0f15188a9258226a0e48801f3f79257713881d6e
jvngarcia <angelgarciaweb@gmail.com>
2024-07-29 11:46:33 -0400

    chore: Remove debug print statements and improve subcommand discovery

--- coral_cli/cli.py
+++ coral_cli/cli.py
@@ -19,15 +19,12 @@ def cli():
   pass
 
 def add_subcommands(maincommand=cli):
-  print(f"Looking for subcommands in {SUBCOMMAND_DIR.glob('*.py')}")
   for modpath in SUBCOMMAND_DIR.glob('*.py'):
-    print(f"Found {modpath}")
     modname = re.sub(f'/', '.',  str(modpath)).rpartition('.py')[0]
     
     # Replace backslash with dot
     modname = modname.replace('\\', '.')
     
-    print(f"Importing {modname}")
     mod = importlib.import_module(modname)
     # Filter out any things that aren't a click Command
     for attr in dir(mod):
